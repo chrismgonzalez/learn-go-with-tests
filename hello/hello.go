a295e849025c1caf1e43018e93989982540de9ca
chrismgonzalez <cmgonza89@gmail.com>
2020-07-18 20:08:09 -0500

    expanded tests, added language support

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,31 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("", ""))
 }
